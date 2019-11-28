2a465f96b8134e0adbe12d981540cb203bfae858
joe <xielamenla@qq.com>
2019-11-29 01:56:01 +0800

    tweak the build scripts

--- api/src/lib.rs
+++ api/src/lib.rs
@@ -1,7 +1,24 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+use std::ffi::{CStr, CString};
+use std::os::raw::c_char;
+
+#[no_mangle]
+pub extern "C" fn rust_hello(to: *const c_char) -> *mut c_char {
+    let c_str = unsafe { CStr::from_ptr(to) };
+    let recipient = match c_str.to_str() {
+        Err(_) => "there",
+        Ok(string) => string,
+    };
+    CString::new("Hello bit 2223333 ".to_owned() + recipient)
+        .unwrap()
+        .into_raw()
+}
+
+#[no_mangle]
+pub extern "C" fn rust_hello_free(s: *mut c_char) {
+    unsafe {
+        if s.is_null() {
+            return;
+        }
+        CString::from_raw(s)
+    };
 }
