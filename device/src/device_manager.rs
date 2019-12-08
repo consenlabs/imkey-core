08f2ba6d18f0a9810b73b4ab25d35de6ddc0fd0b
xiaoguang101010 <xiaoguang101010@163.com>
2019-12-09 00:52:21 +0800

    device bind function commit

---
+++ device/src/device_manager.rs
@@ -0,0 +1,17 @@
+pub struct DeviceManage{}
+
+impl DeviceManage{
+    pub fn get_seid() -> String{
+        //select ISD command
+
+        //get seid command
+        String::from("xxxxxxxxxxxxxxxxxxxxxxx")
+    }
+
+    pub fn get_sn() -> String{
+        //send select ISD command
+
+        //send get sn command
+        String::from("xxxxxxxxxxxxxxxxxxxxxxxxx")
+    }
+}
