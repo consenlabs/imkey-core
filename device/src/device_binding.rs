e10b51e4bd4b32b93a31076889f8af734833ed39
joe <xielamenla@qq.com>
2019-12-24 15:37:43 +0800

    comment hid_api

--- device/src/device_binding.rs
+++ device/src/device_binding.rs
@@ -24,149 +24,150 @@ impl DeviceManage {
         }
     }
     pub fn bind_check(&mut self, file_path: &String) {
-        //获取seid
-        let seid = String::from("18080000000000860001010000000106");
-        //获取SN号
-        let sn = String::from("imKey01191200010");
+        // //获取seid
+        // let seid = String::from("18080000000000860001010000000106");
+        // //获取SN号
+        // let sn = String::from("imKey01191200010");
 
-        //计算文件加密密钥
-        //        let mut temp_key_manager = KeyManager::new();
-        let mut temp_key_manager = &mut self.key_manager;
-        temp_key_manager.gen_encrypt_key(&seid, &sn);
+        // //计算文件加密密钥
+        // //        let mut temp_key_manager = KeyManager::new();
+        // let mut temp_key_manager = &mut self.key_manager;
+        // temp_key_manager.gen_encrypt_key(&seid, &sn);
 
-        //获取本地密钥文件内容
-        let ciphertext = KeyManager::get_key_file_data(file_path, &seid);
-        let mut key_flag = false;
-        if !ciphertext.is_empty() {
-            key_flag = !temp_key_manager.decrypt_keys(ciphertext.as_bytes());
-        }
+        // //获取本地密钥文件内容
+        // let ciphertext = KeyManager::get_key_file_data(file_path, &seid);
+        // let mut key_flag = false;
+        // if !ciphertext.is_empty() {
+        //     key_flag = !temp_key_manager.decrypt_keys(ciphertext.as_bytes());
+        // }
 
-        //如果密钥文件不存在或者密钥文件里没有数据则重新生成
-        if ciphertext.is_empty() || key_flag {
-            //生成公私钥
-            temp_key_manager.gen_local_keys();
-            key_flag = true;
-        }
+        // //如果密钥文件不存在或者密钥文件里没有数据则重新生成
+        // if ciphertext.is_empty() || key_flag {
+        //     //生成公私钥
+        //     temp_key_manager.gen_local_keys();
+        //     key_flag = true;
+        // }
 
-        //生成bindcheck指令
-        let bind_check_apdu =
-            Apdu::bind_check(&temp_key_manager.pub_key.unwrap().as_ref().to_vec());
+        // //生成bindcheck指令
+        // let bind_check_apdu =
+        //     Apdu::bind_check(&temp_key_manager.pub_key.unwrap().as_ref().to_vec());
 
-        //发送bindcheck指令，并获取返回数据 TODO
-        let hid_device = hid_api::connect();
-        let select_imk_applet = Apdu::select_applet("695F696D6B");
-        let response = hid_api::send(&hid_device, &select_imk_applet);
-        let bind_check_apdu_resp_data = hid_api::send(&hid_device, &bind_check_apdu);
+        // //发送bindcheck指令，并获取返回数据 TODO
+        // let hid_device = hid_api::connect();
+        // let select_imk_applet = Apdu::select_applet("695F696D6B");
+        // let response = hid_api::send(&hid_device, &select_imk_applet);
+        // let bind_check_apdu_resp_data = hid_api::send(&hid_device, &bind_check_apdu);
 
-        //获取状态值 //状态 0x00: 未绑定  0x55: 与传入appPK绑定  0xAA：与其他appPK绑定
-        let status: String = bind_check_apdu_resp_data.chars().take(2).collect();
-        let se_pub_key_cert: String = bind_check_apdu_resp_data.chars().skip(2).collect();
-        if status.eq("00") || status.eq("AA") {
-            //验证SE证书 TODO
+        // //获取状态值 //状态 0x00: 未绑定  0x55: 与传入appPK绑定  0xAA：与其他appPK绑定
+        // let status: String = bind_check_apdu_resp_data.chars().take(2).collect();
+        // let se_pub_key_cert: String = bind_check_apdu_resp_data.chars().skip(2).collect();
+        // if status.eq("00") || status.eq("AA") {
+        //     //验证SE证书 TODO
 
-            //解析SE公钥证书，获取SE公钥
-            //82cc68ac4bd131d84d4dcfeab1bb606cae40b9be267892326f3ccfa1a0f862a1
-            //040258df4552fbe4f1eb2b4d2ed30978511e2e3bc1f9eed02502b5ef766a98900124f8ea6965322edb80f45de48058c2b5c2cc5df57e755f9437d5d47d0d14c7d1
-            let temp_se_pub_key = Vec::from_hex("0403089D8A83A87F24D906303A49D39669D17B0F7AB76EB098A65AFEF31154E75DEE5B87B69CBF78F11E831A4961C8A8F031C2869EA0716C798F76F5E91338DC35").unwrap();
-            let mut se_pub_key = [0u8; 65];
-            se_pub_key.copy_from_slice(temp_se_pub_key.as_slice());
-            temp_key_manager.se_pub_key = Some(se_pub_key);
+        //     //解析SE公钥证书，获取SE公钥
+        //     //82cc68ac4bd131d84d4dcfeab1bb606cae40b9be267892326f3ccfa1a0f862a1
+        //     //040258df4552fbe4f1eb2b4d2ed30978511e2e3bc1f9eed02502b5ef766a98900124f8ea6965322edb80f45de48058c2b5c2cc5df57e755f9437d5d47d0d14c7d1
+        //     let temp_se_pub_key = Vec::from_hex("0403089D8A83A87F24D906303A49D39669D17B0F7AB76EB098A65AFEF31154E75DEE5B87B69CBF78F11E831A4961C8A8F031C2869EA0716C798F76F5E91338DC35").unwrap();
+        //     let mut se_pub_key = [0u8; 65];
+        //     se_pub_key.copy_from_slice(temp_se_pub_key.as_slice());
+        //     temp_key_manager.se_pub_key = Some(se_pub_key);
 
-            //协商会话密钥
-            let se_pub_key_obj = PublicKey::from_slice(temp_se_pub_key.as_ref()).unwrap();
-            println!(
-                "pri_key : {:?}",
-                hex::encode_upper(temp_key_manager.pri_key.unwrap().as_ref())
-            );
-            let locl_pri_key_obj =
-                SecretKey::from_slice(temp_key_manager.pri_key.unwrap().as_ref()).unwrap();
-            let sec = SharedSecret::new(&se_pub_key_obj, &locl_pri_key_obj);
-            //SHA1
-            let sha1_data = Sha1::from(&sec[..]).digest().bytes();
-            //设置session key
-            let mut temp_session_key = [0u8; 16];
-            temp_session_key.copy_from_slice(&sha1_data[..16]);
-            println!(
-                "sessionkey : {:?}",
-                hex::encode_upper(temp_session_key.to_vec())
-            );
-            temp_key_manager.session_key = Some(temp_session_key);
+        //     //协商会话密钥
+        //     let se_pub_key_obj = PublicKey::from_slice(temp_se_pub_key.as_ref()).unwrap();
+        //     println!(
+        //         "pri_key : {:?}",
+        //         hex::encode_upper(temp_key_manager.pri_key.unwrap().as_ref())
+        //     );
+        //     let locl_pri_key_obj =
+        //         SecretKey::from_slice(temp_key_manager.pri_key.unwrap().as_ref()).unwrap();
+        //     let sec = SharedSecret::new(&se_pub_key_obj, &locl_pri_key_obj);
+        //     //SHA1
+        //     let sha1_data = Sha1::from(&sec[..]).digest().bytes();
+        //     //设置session key
+        //     let mut temp_session_key = [0u8; 16];
+        //     temp_session_key.copy_from_slice(&sha1_data[..16]);
+        //     println!(
+        //         "sessionkey : {:?}",
+        //         hex::encode_upper(temp_session_key.to_vec())
+        //     );
+        //     temp_key_manager.session_key = Some(temp_session_key);
 
-            //保存密钥到本地文件
-            if key_flag {
-                let ciphertext = temp_key_manager.encrypt_data();
-                KeyManager::save_keys_to_local_file(&ciphertext, file_path, &seid);
-            }
-        }
+        //     //保存密钥到本地文件
+        //     if key_flag {
+        //         let ciphertext = temp_key_manager.encrypt_data();
+        //         KeyManager::save_keys_to_local_file(&ciphertext, file_path, &seid);
+        //     }
+        // }
     }
 
     pub fn bind_acquire(&self, binding_code: &String) -> String {
-        let temp_binding_code = binding_code.to_uppercase();
-        let binding_code_bytes = temp_binding_code.as_bytes();
-        //绑定码校验 TODO
-        let reg_ex = "^[A-HJ-NP-Z2-9]{8}$";
+        // let temp_binding_code = binding_code.to_uppercase();
+        // let binding_code_bytes = temp_binding_code.as_bytes();
+        // //绑定码校验 TODO
+        // let reg_ex = "^[A-HJ-NP-Z2-9]{8}$";
 
-        //RSA加密绑定码
-        let auth_code_ciphertext = auth_code_encrypt(&temp_binding_code);
+        // //RSA加密绑定码
+        // let auth_code_ciphertext = auth_code_encrypt(&temp_binding_code);
 
-        //保存绑定码 TODO
-        //        let seid = String::from("18090000000000860001010000000204");
-        //        let auth_code_storage_result = auth_code_storage_request::build_request_data(seid, auth_code_ciphertext).auth_code_storage();
-        //        if auth_code_storage_result.is_err() {
-        //            //TODO
-        //        }
+        // //保存绑定码 TODO
+        // //        let seid = String::from("18090000000000860001010000000204");
+        // //        let auth_code_storage_result = auth_code_storage_request::build_request_data(seid, auth_code_ciphertext).auth_code_storage();
+        // //        if auth_code_storage_result.is_err() {
+        // //            //TODO
+        // //        }
 
-        //选择IMK applet TODO
-        //        select_imk_applet();
-        let hid_device = hid_api::connect();
-        let select_imk_applet = Apdu::select_applet("695F696D6B");
-        let response = hid_api::send(&hid_device, &select_imk_applet);
+        // //选择IMK applet TODO
+        // //        select_imk_applet();
+        // let hid_device = hid_api::connect();
+        // let select_imk_applet = Apdu::select_applet("695F696D6B");
+        // let response = hid_api::send(&hid_device, &select_imk_applet);
 
-        //计算HASH
-        let mut data: Vec<u8> = Vec::new();
-        data.extend(binding_code_bytes);
-        println!("{}", hex::encode_upper(binding_code_bytes));
-        data.extend(self.key_manager.pub_key.unwrap().as_ref().iter());
-        println!(
-            "pub_key:{:?}",
-            hex::encode_upper(self.key_manager.pub_key.unwrap().as_ref())
-        );
+        // //计算HASH
+        // let mut data: Vec<u8> = Vec::new();
+        // data.extend(binding_code_bytes);
+        // println!("{}", hex::encode_upper(binding_code_bytes));
+        // data.extend(self.key_manager.pub_key.unwrap().as_ref().iter());
+        // println!(
+        //     "pub_key:{:?}",
+        //     hex::encode_upper(self.key_manager.pub_key.unwrap().as_ref())
+        // );
 
-        data.extend(self.key_manager.se_pub_key.unwrap().iter());
-        println!(
-            "se_pub_key:{:?}",
-            hex::encode_upper(self.key_manager.se_pub_key.unwrap().as_ref())
-        );
-        println!("data : {:?}", hex::encode_upper(data.as_slice()));
-        let data_hash = digest::digest(&digest::SHA256, data.as_slice());
-        println!("hash value:{:?}", data_hash.as_ref());
-        println!("data : {:?}", hex::encode_upper(data_hash.as_ref()));
+        // data.extend(self.key_manager.se_pub_key.unwrap().iter());
+        // println!(
+        //     "se_pub_key:{:?}",
+        //     hex::encode_upper(self.key_manager.se_pub_key.unwrap().as_ref())
+        // );
+        // println!("data : {:?}", hex::encode_upper(data.as_slice()));
+        // let data_hash = digest::digest(&digest::SHA256, data.as_slice());
+        // println!("hash value:{:?}", data_hash.as_ref());
+        // println!("data : {:?}", hex::encode_upper(data_hash.as_ref()));
 
-        //用sessionKey加密HASH值
-        type Aes128Cbc = Cbc<Aes128, Pkcs7>;
-        println!(
-            "session_key:{:?}",
-            hex::encode_upper(self.key_manager.session_key.unwrap().as_ref())
-        );
-        println!("iv : {:?}", hex::encode_upper(gen_iv(&temp_binding_code)));
+        // //用sessionKey加密HASH值
+        // type Aes128Cbc = Cbc<Aes128, Pkcs7>;
+        // println!(
+        //     "session_key:{:?}",
+        //     hex::encode_upper(self.key_manager.session_key.unwrap().as_ref())
+        // );
+        // println!("iv : {:?}", hex::encode_upper(gen_iv(&temp_binding_code)));
 
-        let cipher = Aes128Cbc::new_var(
-            self.key_manager.session_key.unwrap().as_ref(),
-            &gen_iv(&temp_binding_code).as_ref(),
-        )
-        .unwrap();
-        let ciphertext = cipher.encrypt_vec(data_hash.as_ref());
-        println!("ciphertext:{:?}", hex::encode_upper(ciphertext.as_slice()));
-        //生成identityVerify指令数据
-        let mut apdu_data = Vec::new();
-        apdu_data.extend(self.key_manager.pub_key.unwrap().as_ref());
-        apdu_data.extend(ciphertext);
-        println!("{:?}", apdu_data.as_slice());
-        let identity_verify_apdu = Apdu::identity_verify(&apdu_data);
-        println!("{:?}", identity_verify_apdu);
-        //发送指令到设备
-        let response = hid_api::send(&hid_device, &identity_verify_apdu);
-        response.chars().take(2).collect()
+        // let cipher = Aes128Cbc::new_var(
+        //     self.key_manager.session_key.unwrap().as_ref(),
+        //     &gen_iv(&temp_binding_code).as_ref(),
+        // )
+        // .unwrap();
+        // let ciphertext = cipher.encrypt_vec(data_hash.as_ref());
+        // println!("ciphertext:{:?}", hex::encode_upper(ciphertext.as_slice()));
+        // //生成identityVerify指令数据
+        // let mut apdu_data = Vec::new();
+        // apdu_data.extend(self.key_manager.pub_key.unwrap().as_ref());
+        // apdu_data.extend(ciphertext);
+        // println!("{:?}", apdu_data.as_slice());
+        // let identity_verify_apdu = Apdu::identity_verify(&apdu_data);
+        // println!("{:?}", identity_verify_apdu);
+        // //发送指令到设备
+        // let response = hid_api::send(&hid_device, &identity_verify_apdu);
+        // response.chars().take(2).collect()
+        String::from("s: &str")
     }
 }
 
@@ -209,10 +210,11 @@ fn auth_code_encrypt(auth_code: &String) -> String {
 }
 
 pub fn display_bind_code() -> String {
-    let hid_device = hid_api::connect();
-    let select_imk_applet = Apdu::select_applet("695F696D6B");
-    let response = hid_api::send(&hid_device, &select_imk_applet);
-    let gen_auth_code_apdu = Apdu::generate_auth_code();
-    let bind_code = hid_api::send(&hid_device, &gen_auth_code_apdu);
-    bind_code
+    // let hid_device = hid_api::connect();
+    // let select_imk_applet = Apdu::select_applet("695F696D6B");
+    // let response = hid_api::send(&hid_device, &select_imk_applet);
+    // let gen_auth_code_apdu = Apdu::generate_auth_code();
+    // let bind_code = hid_api::send(&hid_device, &gen_auth_code_apdu);
+    // bind_code
+    String::from("s: &str")
 }
