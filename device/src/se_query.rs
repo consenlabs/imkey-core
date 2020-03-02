57e66fb57757ab865b37f4a98b0238af35b3f624
wyoyw <wyoyw@126.com>
2020-03-02 16:50:28 +0800

    Optimization device manager function

--- device/src/se_query.rs
+++ device/src/se_query.rs
@@ -2,6 +2,10 @@ use common::constants::{TSM_ACTION_SE_QUERY, TSM_RETURN_CODE_SUCCESS};
 use common::{error::ImkeyError, https};
 use serde::{Deserialize, Serialize};
 
+extern crate android_logger;
+use log::Level;
+use android_logger::Config;
+
 #[derive(Debug, Serialize, Deserialize)]
 pub struct se_query_request {
     pub seid: String,
@@ -24,7 +28,18 @@ pub struct service_response {
 pub struct se_query_response {
     pub seid: Option<String>,
     pub nextStepKey: Option<String>,
-    pub apduList: Option<Vec<String>>,
+    pub sn: Option<String>,
+    pub sdkMode: Option<String>,
+    pub availableAppBeanList: Option<Vec<available_app_bean>>,
+}
+#[derive(Serialize, Deserialize, Debug)]
+pub struct available_app_bean {
+    pub appLogo: Option<String>,
+    pub installMode: Option<String>,
+    pub installedVersion: Option<String>,
+    pub instanceAid: Option<String>,
+    pub lastUpdated: Option<String>,
+    pub latestVersion: Option<String>,
 }
 
 impl se_query_request {
@@ -49,9 +64,16 @@ impl se_query_request {
         let req_data = serde_json::to_vec_pretty(&self).unwrap();
         let mut response_data = https::post(TSM_ACTION_SE_QUERY, req_data);
         let return_bean: service_response =
-            serde_json::from_str(response_data.ok().unwrap().as_str())
+            serde_json::from_str(response_data.ok().unwrap().as_str().clone())
                 .expect("imkey message seriailize error");
         println!("反馈报文：{:#?}", return_bean);
+        android_logger::init_once(
+            Config::default().with_min_level(Level::Trace),
+        );
+
+        info!("se_query this is a debug {:#?}", return_bean);
+        //info!("se_query this is a debug {:#?}", return_bean);
+
         if return_bean._ReturnCode == TSM_RETURN_CODE_SUCCESS {
             //判断步骤key是否已经结束
             //            let next_step_key = return_bean._ReturnData.nextStepKey.unwrap();
