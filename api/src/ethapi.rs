cf302858d3d45eb5a10aa09598fa00048666f030
joe <xielamenla@qq.com>
2020-03-09 22:15:45 +0800

    some generated code(should ignore

--- api/src/ethapi.rs
+++ api/src/ethapi.rs
@@ -1,4 +1,3 @@
-/// FUNCTION: sign_tx(SignParam{input: EthTxInput}): EthTxOutput
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct EthTxInput {
     #[prost(string, tag="1")]
@@ -11,10 +10,10 @@ pub struct EthTxInput {
     pub to: std::string::String,
     #[prost(string, tag="5")]
     pub value: std::string::String,
-    #[prost(bytes, tag="6")]
-    pub data: std::vec::Vec<u8>,
-    #[prost(uint64, tag="7")]
-    pub chain_id: u64,
+    #[prost(string, tag="6")]
+    pub data: std::string::String,
+    #[prost(string, tag="7")]
+    pub chain_id: std::string::String,
     #[prost(string, tag="8")]
     pub path: std::string::String,
     #[prost(string, tag="9")]
@@ -25,8 +24,6 @@ pub struct EthTxInput {
     pub sender: std::string::String,
     #[prost(string, tag="12")]
     pub fee: std::string::String,
-    #[prost(string, tag="13")]
-    pub raw_data: std::string::String,
 }
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct EthTxOutput {
@@ -39,4 +36,18 @@ pub struct EthTxOutput {
 pub struct EthAddressResponse {
     #[prost(string, tag="1")]
     pub address: std::string::String,
+}
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct EthPersonalSignInput {
+    #[prost(string, tag="1")]
+    pub path: std::string::String,
+    #[prost(string, tag="2")]
+    pub message: std::string::String,
+    #[prost(string, tag="3")]
+    pub sender: std::string::String,
+}
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct EthPersonalSignOutput {
+    #[prost(string, tag="1")]
+    pub signature: std::string::String,
 }
