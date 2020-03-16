79072db9df34a73e9b7cf4ba7abeca9dfc88e5d4
xiaoguang <xiaoguang101010@163.com>
2020-03-16 12:40:27 +0800

    fix segwit transaction question

--- common/src/apdu.rs
+++ common/src/apdu.rs
@@ -295,7 +295,7 @@ impl BtcApdu {
                 temp_apdu_vec.push(0x80);
                 temp_apdu_vec.push(ins);
                 temp_apdu_vec.push(p1);
-                temp_apdu_vec.push(0x80);
+                temp_apdu_vec.push(0x00);
                 temp_apdu_vec.push(LC_MAX as u8);
                 temp_apdu_vec.extend_from_slice(
                     &data[index * LC_MAX as usize..((index + 1) * LC_MAX as usize) as usize],
