67933737b9969581a317076ddb15130a365301c4
joe <xielamenla@qq.com>
2020-03-18 08:20:07 +0800

    sign auto test

--- ios/imKeyConnector/Classes/Proto/eos.pb.swift
+++ ios/imKeyConnector/Classes/Proto/eos.pb.swift
@@ -60,6 +60,18 @@ public struct Eosapi_EosTxOutput {
   // `Message` and `Message+*Additions` files in the SwiftProtobuf library for
   // methods supported on all messages.
 
+  public var transMultiSigns: [Eosapi_EosSignResult] = []
+
+  public var unknownFields = SwiftProtobuf.UnknownStorage()
+
+  public init() {}
+}
+
+public struct Eosapi_EosSignResult {
+  // SwiftProtobuf.Message conformance is added in an extension below. See the
+  // `Message` and `Message+*Additions` files in the SwiftProtobuf library for
+  // methods supported on all messages.
+
   public var hash: String = String()
 
   public var signs: [String] = []
@@ -224,6 +236,35 @@ extension Eosapi_EosTxInput: SwiftProtobuf.Message, SwiftProtobuf._MessageImplem
 extension Eosapi_EosTxOutput: SwiftProtobuf.Message, SwiftProtobuf._MessageImplementationBase, SwiftProtobuf._ProtoNameProviding {
   public static let protoMessageName: String = _protobuf_package + ".EosTxOutput"
   public static let _protobuf_nameMap: SwiftProtobuf._NameMap = [
+    1: .standard(proto: "trans_multi_signs"),
+  ]
+
+  public mutating func decodeMessage<D: SwiftProtobuf.Decoder>(decoder: inout D) throws {
+    while let fieldNumber = try decoder.nextFieldNumber() {
+      switch fieldNumber {
+      case 1: try decoder.decodeRepeatedMessageField(value: &self.transMultiSigns)
+      default: break
+      }
+    }
+  }
+
+  public func traverse<V: SwiftProtobuf.Visitor>(visitor: inout V) throws {
+    if !self.transMultiSigns.isEmpty {
+      try visitor.visitRepeatedMessageField(value: self.transMultiSigns, fieldNumber: 1)
+    }
+    try unknownFields.traverse(visitor: &visitor)
+  }
+
+  public static func ==(lhs: Eosapi_EosTxOutput, rhs: Eosapi_EosTxOutput) -> Bool {
+    if lhs.transMultiSigns != rhs.transMultiSigns {return false}
+    if lhs.unknownFields != rhs.unknownFields {return false}
+    return true
+  }
+}
+
+extension Eosapi_EosSignResult: SwiftProtobuf.Message, SwiftProtobuf._MessageImplementationBase, SwiftProtobuf._ProtoNameProviding {
+  public static let protoMessageName: String = _protobuf_package + ".EosSignResult"
+  public static let _protobuf_nameMap: SwiftProtobuf._NameMap = [
     1: .same(proto: "hash"),
     2: .same(proto: "signs"),
   ]
@@ -248,7 +289,7 @@ extension Eosapi_EosTxOutput: SwiftProtobuf.Message, SwiftProtobuf._MessageImple
     try unknownFields.traverse(visitor: &visitor)
   }
 
-  public static func ==(lhs: Eosapi_EosTxOutput, rhs: Eosapi_EosTxOutput) -> Bool {
+  public static func ==(lhs: Eosapi_EosSignResult, rhs: Eosapi_EosSignResult) -> Bool {
     if lhs.hash != rhs.hash {return false}
     if lhs.signs != rhs.signs {return false}
     if lhs.unknownFields != rhs.unknownFields {return false}
