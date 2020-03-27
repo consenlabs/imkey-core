9fa55a43a926dfe1b3b679cc997457c41eb6b079
wyoyw <wyoyw@126.com>
2020-03-27 10:37:47 +0800

    device manager api optimization

--- api/src/device_manager.rs
+++ api/src/device_manager.rs
@@ -1,53 +1,45 @@
 use crate::api::DeviceParam;
-use crate::deviceapi::{AppAction, SdkInfoResponse, BindCheckResponse};
+use crate::deviceapi::{AppDownloadReq, AppUpdateReq, AppDeleteReq,
+                       CheckUpdateRes, AvailableAppBean, BindCheckReq, BindCheckRes, BindAcquireReq,
+                       BindAcquireRes, GetSeidRes, GetSnRes, GetRamSizeRes, GetFirmwareVersionRes,
+                       GetBatteryPowerRes, GetLifeTimeRes, GetBleNameRes, SetBleNameReq,
+                       GetBleVersionRes, GetSdkInfoRes};
 use crate::deviceapi::EmptyResponse;
-use crate::deviceapi::{
-    BindAcquire, BindAcquireResponse, BindCheck, ApduResponse
-};
-use crate::deviceapi::{CheckUpdateResponse, AvailableAppBean, BleAction};
 use crate::wallet_handler::encode_message;
 use common::constants;
 use common::applet;
-use device::device_binding::{display_bind_code, DeviceManage};
 use prost::Message;
 use device::manager;
 use crate::error_handling::Result;
 
-pub fn device_app_delete(param: &DeviceParam) -> Result<Vec<u8>> {
-    let app_action: AppAction =
-        AppAction::decode(&param.param.as_ref().expect("imkey_illegal_param").value.clone())
-            .expect("imkey_illegal_param");
-    manager::app_delete(app_action.app_name.as_ref())?;
+pub fn app_download(data: &[u8]) -> Result<Vec<u8>> {
+    let request: AppDownloadReq = AppDownloadReq::decode(data).expect("imkey_illegal_param");
+    manager::app_download(request.app_name.as_ref())?;
     let response_msg = EmptyResponse {};
     encode_message(response_msg)
 }
 
-pub fn device_app_download(param: &DeviceParam) -> Result<Vec<u8>> {
-
-    let app_action: AppAction =
-        AppAction::decode(&param.param.as_ref().expect("imkey_illegal_param").value.clone())
-            .expect("imkey_illegal_param");
-    manager::app_download(app_action.app_name.as_ref())?;
+pub fn app_update(data: &[u8]) -> Result<Vec<u8>> {
+    let request: AppUpdateReq = AppUpdateReq::decode(data).expect("imkey_illegal_prarm");
+    manager::app_update(request.app_name.as_ref())?;
     let response_msg = EmptyResponse {};
     encode_message(response_msg)
 }
 
-pub fn device_app_update(param: &DeviceParam) -> Result<Vec<u8>> {
-    let app_action: AppAction =
-        AppAction::decode(&param.param.as_ref().expect("imkey_illegal_prarm").value.clone())
-            .expect("imkey_illegal_prarm");
-    manager::app_update(app_action.app_name.as_ref())?;
+pub fn app_delete(data: &[u8]) -> Result<Vec<u8>> {
+    let request: AppDeleteReq = AppDeleteReq::decode(data).expect("imkey_illegal_param");
+    manager::app_delete(request.app_name.as_ref())?;
     let response_msg = EmptyResponse {};
     encode_message(response_msg)
 }
 
-pub fn device_activate(_param: &DeviceParam) -> Result<Vec<u8>> {
+pub fn se_activate() -> Result<Vec<u8>> {
     manager::active_device()?;
     let response_msg = EmptyResponse {};
     encode_message(response_msg)
 }
 
-pub fn device_query(_param: &DeviceParam) -> Result<Vec<u8>> {
+pub fn check_update() -> Result<Vec<u8>> {
 
     let response = manager::check_update()?;
 
@@ -76,7 +68,7 @@ pub fn device_query(_param: &DeviceParam) -> Result<Vec<u8>> {
         status = constants::IMKEY_DEV_STATUS_INACTIVATED;
     }
 
-    let response_msg = CheckUpdateResponse {
+    let response_msg = CheckUpdateRes {
         se_id: response._ReturnData.seid.unwrap(),
         sn: response._ReturnData.sn.unwrap(),
         status : status.to_string(),
@@ -86,137 +78,128 @@ pub fn device_query(_param: &DeviceParam) -> Result<Vec<u8>> {
     encode_message(response_msg)
 }
 
-pub fn device_secure_check(_param: &DeviceParam) -> Result<Vec<u8>> {
+pub fn se_secure_check() -> Result<Vec<u8>> {
     manager::check_device()?;
     let response_msg = EmptyResponse {};
     encode_message(response_msg)
 }
 
-pub fn device_bind_check(param: &DeviceParam) -> Result<Vec<u8>> {
-    let bind_check: BindCheck =
-        BindCheck::decode(&param.param.as_ref().expect("imkey_illegal_param").value.clone())
-            .expect("imkey_illegal_param");
-    let check_result = DeviceManage::bind_check(&bind_check.file_path)?;
-    let response_msg = BindCheckResponse{
+pub fn bind_check(data: &[u8]) -> Result<Vec<u8>> {
+    let bind_check: BindCheckReq = BindCheckReq::decode(data).expect("imkey_illegal_param");
+    let check_result = manager::bind_check(&bind_check.file_path)?;
+    let response_msg = BindCheckRes{
         bind_status: check_result
     };
     encode_message(response_msg)
 }
 
-pub fn device_bind_acquire(param: &DeviceParam) -> Result<Vec<u8>> {
-    let bind_acquire: BindAcquire =
-        BindAcquire::decode(&param.param.as_ref().expect("imkey_illegal_param").value.clone())
-            .expect("imkey_illegal_param");
-    let bind_result = DeviceManage::bind_acquire(&bind_acquire.bind_code)?;
-    let response_msg = BindAcquireResponse {
+pub fn bind_display_code() -> Result<Vec<u8>> {
+    manager::bind_display_code()?;
+    let response_msg = EmptyResponse {};
+    encode_message(response_msg)
+}
+
+pub fn bind_acquire(data: &[u8]) -> Result<Vec<u8>> {
+    let bind_acquire: BindAcquireReq = BindAcquireReq::decode(data).expect("imkey_illegal_param");
+    let bind_result = manager::bind_acquire(&bind_acquire.bind_code)?;
+    let response_msg = BindAcquireRes {
         bind_result,
     };
     encode_message(response_msg)
 }
 
-pub fn device_display_bind_code(_param: &DeviceParam) -> Result<Vec<u8>> {
-    display_bind_code()?;
+pub fn get_seid() -> Result<Vec<u8>> {
+
+    let seid = manager::get_se_id().ok().expect("get_seid_error");
+    let response_msg = GetSeidRes {
+        seid,
+    };
+    encode_message(response_msg)
+}
+
+pub fn get_sn() -> Result<Vec<u8>> {
+
+    let sn = manager::get_sn().ok().expect("get_sn_error");
+    let response_msg = GetSnRes {
+        sn,
+    };
+    encode_message(response_msg)
+}
+
+pub fn get_ram_size() -> Result<Vec<u8>> {
+
+    let ram_size = manager::get_ram_size().ok().expect("get_ram_size_error");
+    let response_msg = GetRamSizeRes {
+        ram_size,
+    };
+    encode_message(response_msg)
+}
+
+pub fn get_firmware_version() -> Result<Vec<u8>> {
+
+    let firmware_version = manager::get_firmware_version().ok().expect("get_firmware_version_error");
+
+    let response_msg = GetFirmwareVersionRes {
+        firmware_version,
+    };
+    encode_message(response_msg)
+}
+
+pub fn get_battery_power() -> Result<Vec<u8>> {
+
+    let battery_power = manager::get_battery_power().ok().expect("get_battery_power_error");
+    let response_msg = GetBatteryPowerRes {
+        battery_power,
+    };
+    encode_message(response_msg)
+}
+
+pub fn get_life_time() -> Result<Vec<u8>> {
+
+    let life_time = manager::get_life_time().ok().expect("get_life_time_error");
+    let response_msg = GetLifeTimeRes {
+        life_time,
+    };
+    encode_message(response_msg)
+}
+
+pub fn get_ble_name() -> Result<Vec<u8>> {
+
+    let ble_name = manager::get_ble_name().ok().expect("get_ble_name_error");
+
+    let response_msg = GetBleNameRes {
+        ble_name,
+    };
+    encode_message(response_msg)
+}
+
+pub fn set_ble_name(data: &[u8]) -> Result<Vec<u8>> {
+
+    let request: SetBleNameReq = SetBleNameReq::decode(data).expect("ble_action");
+
+    manager::set_ble_name(request.ble_name).ok().expect("set_ble_name_error");
     let response_msg = EmptyResponse {};
     encode_message(response_msg)
 }
 
+pub fn get_ble_version() -> Result<Vec<u8>> {
 
-pub fn get_seid(_param: &DeviceParam) -> Result<Vec<u8>> {
-
-    let result = manager::get_se_id().ok().expect("get_seid_error");
-    let response_msg = ApduResponse {
-        result,
+    let ble_version = manager::get_ble_version().ok().expect("get_ble_version_error");
+    let response_msg = GetBleVersionRes {
+        ble_version,
     };
     encode_message(response_msg)
 }
 
-pub fn get_sn(_param: &DeviceParam) -> Result<Vec<u8>> {
-
-    let result = manager::get_sn().ok().expect("get_sn_error");
-    let response_msg = ApduResponse {
-        result,
-    };
-    encode_message(response_msg)
-}
-
-pub fn get_ram_size(_param: &DeviceParam) -> Result<Vec<u8>> {
-
-    let result = manager::get_ram_size().ok().expect("get_ram_size_error");
-    let response_msg = ApduResponse {
-        result,
-    };
-    encode_message(response_msg)
-}
-
-pub fn get_firmware_version(_param: &DeviceParam) -> Result<Vec<u8>> {
-
-    let result = manager::get_firmware_version().ok().expect("get_firmware_version_error");
-
-    let response_msg = ApduResponse {
-        result,
-    };
-    encode_message(response_msg)
-}
-
-pub fn get_battery_power(_param: &DeviceParam) -> Result<Vec<u8>> {
-
-    let result = manager::get_battery_power().ok().expect("get_battery_power_error");
-    let response_msg = ApduResponse {
-        result,
-    };
-    encode_message(response_msg)
-}
-
-pub fn get_life_time(_param: &DeviceParam) -> Result<Vec<u8>> {
-
-    let result = manager::get_life_time().ok().expect("get_life_time_error");
-    let response_msg = ApduResponse {
-        result,
-    };
-    encode_message(response_msg)
-}
-
-pub fn get_ble_name(_param: &DeviceParam) -> Result<Vec<u8>> {
-
-    let result = manager::get_ble_name().ok().expect("get_ble_name_error");
-
-    let response_msg = ApduResponse {
-        result,
-    };
-    encode_message(response_msg)
-}
-
-pub fn set_ble_name(param: &DeviceParam) -> Result<Vec<u8>> {
-
-    let ble_action: BleAction =
-        BleAction::decode(&param.param.as_ref().expect("device_param").value.clone())
-            .expect("ble_action");
-
-    let result = manager::set_ble_name(ble_action.ble_name).ok().expect("set_ble_name_error");
-    let response_msg = ApduResponse {
-        result,
-    };
-    encode_message(response_msg)
-}
-
-pub fn get_ble_version(_param: &DeviceParam) -> Result<Vec<u8>> {
-
-    let result = manager::get_ble_version().ok().expect("get_ble_version_error");
-    let response_msg = ApduResponse {
-        result,
-    };
-    encode_message(response_msg)
-}
-
-pub fn get_sdk_info(_param: &DeviceParam) -> Result<Vec<u8>> {
-    let response_msg = SdkInfoResponse {
+pub fn get_sdk_info() -> Result<Vec<u8>> {
+    let response_msg = GetSdkInfoRes {
         sdk_version: constants::VERSION.to_string(),
     };
     encode_message(response_msg)
 }
 
 #[cfg(any(target_os = "macos", target_os = "windows"))]
-pub fn cos_upgrade(_param: &DeviceParam) -> Result<Vec<u8>> {
+pub fn cos_update() -> Result<Vec<u8>> {
     manager::cos_upgrade()?;
     let response_msg = EmptyResponse {};
     encode_message(response_msg)
--- proto/build.rs
+++ proto/build.rs
@@ -7,19 +7,19 @@ fn main() {
     prost_build::compile_protos(&["src/api.proto"], &["src/"]).unwrap();
 
     // tcx-eth
-    env::set_var("OUT_DIR", "../api/src");
+    env::set_var("OUT_DIR", "../wallet/coin-ethereum/src");
     prost_build::compile_protos(&["src/eth.proto"], &["src/"]).unwrap();
 
     // tcx-btc
-    env::set_var("OUT_DIR", "../api/src");
+    env::set_var("OUT_DIR", "../wallet/coin-bitcoin/src");
     prost_build::compile_protos(&["src/btc.proto"], &["src/"]).unwrap();
 
     // tcx-eos
-    env::set_var("OUT_DIR", "../api/src");
+    env::set_var("OUT_DIR", "../wallet/coin-eos/src");
     prost_build::compile_protos(&["src/eos.proto"], &["src/"]).unwrap();
 
     // tcx-cosmos
-    env::set_var("OUT_DIR", "../api/src");
+    env::set_var("OUT_DIR", "../wallet/coin-cosmos/src");
     prost_build::compile_protos(&["src/cosmos.proto"], &["src/"]).unwrap();
 
     // device
@@ -41,19 +41,19 @@ mod tests {
         prost_build::compile_protos(&["src/api.proto"], &["src/"]).unwrap();
 
         // tcx-eth
-        env::set_var("OUT_DIR", "../api/src");
+        env::set_var("OUT_DIR", "../wallet/coin-ethereum/src");
         prost_build::compile_protos(&["src/eth.proto"], &["src/"]).unwrap();
 
         // tcx-btc
-        env::set_var("OUT_DIR", "../api/src");
+        env::set_var("OUT_DIR", "../wallet/coin-bitcoin/src");
         prost_build::compile_protos(&["src/btc.proto"], &["src/"]).unwrap();
 
         // tcx-eos
-        env::set_var("OUT_DIR", "../api/src");
+        env::set_var("OUT_DIR", "../wallet/coin-eos/src");
         prost_build::compile_protos(&["src/eos.proto"], &["src/"]).unwrap();
 
         // tcx-cosmos
-        env::set_var("OUT_DIR", "../api/src");
+        env::set_var("OUT_DIR", "../wallet/coin-cosmos/src");
         prost_build::compile_protos(&["src/cosmos.proto"], &["src/"]).unwrap();
 
         // device
