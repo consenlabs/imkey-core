7b42bb601ff12bdd54ecd743a08d90f456d64575
xiaoguang <xiaoguang101010@163.com>
2020-05-08 10:09:44 +0800

    delete crypto and connector crate

--- connector/build-ios.sh
+++
@@ -1,6 +0,0 @@
-# cargo clean
-cargo lipo --release
-cbindgen src/lib.rs -l c > target/connector.h
-
-cp target/universal/release/libconnector.a ../ios/imKeyConnector/Classes/include/libconnector.a
-cp target/connector.h ../ios/imKeyConnector/Classes/include/connector.h
