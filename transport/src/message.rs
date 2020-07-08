5d44269ea635ae5f28ee226084dc9af83284516e
xiaoguang <xiaoguang101010@163.com>
2020-07-08 17:01:58 +0800

    Increase test case coverage

--- transport/src/message.rs
+++ transport/src/message.rs
@@ -35,49 +35,6 @@ pub fn set_callback(callback: extern "C" fn(apdu: *const c_char, timeout: i32) -
     *_callback = callback;
 }
 
-#[no_mangle]
-pub extern "C" fn rust_hello(
-    to: *const c_char,
-    callback: extern "C" fn(apdu: *const c_char) -> *const c_char,
-) -> *mut c_char {
-    let _cb = callback;
-    let c_str = unsafe { CStr::from_ptr(to) };
-    let recipient = match c_str.to_str() {
-        Err(_) => "there",
-        Ok(string) => string,
-    };
-    callback(
-        CString::new("Hello bit 2223333 ".to_owned() + recipient)
-            .unwrap()
-            .into_raw(),
-    );
-    CString::new("Hello bit 2223333 ".to_owned() + recipient)
-        .unwrap()
-        .into_raw()
-}
-
-#[no_mangle]
-pub extern "C" fn rust_hello_free(s: *mut c_char) {
-    unsafe {
-        if s.is_null() {
-            return;
-        }
-        CString::from_raw(s)
-    };
-}
-
-#[no_mangle]
-pub extern "C" fn get_se_id(
-    callback: extern "C" fn(apdu: *const c_char) -> *const c_char,
-) -> *const c_char {
-    callback(CString::new("00A4040000".to_owned()).unwrap().into_raw());
-    callback(
-        CString::new("80CB800005DFFF028101".to_owned())
-            .unwrap()
-            .into_raw(),
-    )
-}
-
 pub fn get_apdu() -> *const c_char {
     let apdu = APDU.read().unwrap();
     return CString::new(apdu.to_owned()).unwrap().into_raw();
@@ -198,31 +155,6 @@ pub fn send_apdu_timeout(apdu: String, timeout: i32) -> Result<String> {
         };
     }
 }
-//
-//#[test]
-//fn test_str() {
-//    let mut string = STRING.lock().unwrap();
-//
-//    // string.push_str("hello");
-//    *string = String::from("dereferenced22");
-//    //    std::mem::drop(string);
-//
-//    let str2 = STRING.lock().unwrap();
-//    // str2.push_str("hah");
-//    // println!("{}", str2);
-//
-//    if *str2 == "" {
-//        println!("isnull {}", str2);
-//    } else {
-//        println!("{}", str2);
-//    }
-//
-//    set_apdu_r("test".to_string());
-//
-//    let hello = "hello123456".to_string();
-//    let s: String = hello.chars().skip(hello.len() - 4).take(4).collect();
-//    println!("s:{}", s);
-//}
 
 #[test]
 fn test_rwlock() {
