015d7f9a8b48a62f74c9eba6e42b976a6a4e3a68
Neal Xu <imxuneal@gmail.com>
2020-11-05 18:15:38 +0800

    fix: display address not append index

--- api/src/btc_address.rs
+++ api/src/btc_address.rs
@@ -114,7 +114,8 @@ pub fn display_btc_legacy_address(param: &AddressParam) -> Result<Vec<u8>> {
         _ => Network::Testnet,
     };
 
-    let address = BtcAddress::display_address(network, param.path.as_ref())?;
+    let path = format!("{}/0/0", param.path);
+    let address = BtcAddress::display_address(network, &path)?;
 
     let address_message = AddressResult {
         address,
@@ -131,7 +132,8 @@ pub fn display_segwit_address(param: &AddressParam) -> Result<Vec<u8>> {
         _ => Network::Testnet,
     };
 
-    let address = BtcAddress::display_segwit_address(network, param.path.as_ref())?;
+    let path = format!("{}/0/0", param.path);
+    let address = BtcAddress::display_segwit_address(network, &path)?;
 
     let address_message = AddressResult {
         path: param.path.to_string(),
--- common/src/applet.rs
+++ common/src/applet.rs
@@ -5,6 +5,8 @@ pub fn get_appname_by_instid(instid: &str) -> Option<&str> {
         "695F656F73" => Some("EOS"),
         "695F636F736D6F73" => Some("COSMOS"),
         "695F6B315F66696C" => Some("FILECOIN"),
+        "695F6B315F66696C02" => Some("FILECOIN2"),
+        "695F6B315F66696C03" => Some("FILECOIN3"),
         "695F696D6B" => Some("IMK"),
         _ => None,
     }
@@ -16,6 +18,8 @@ pub fn get_instid_by_appname(appname: &str) -> Option<&str> {
         "EOS" => Some("695F656F73"),
         "COSMOS" => Some("695F636F736D6F73"),
         "FILECOIN" => Some("695F6B315F66696C"),
+        "FILECOIN2" => Some("695F6B315F66696C02"),
+        "FILECOIN3" => Some("695F6B315F66696C03"),
         "IMK" => Some("695F696D6B"),
         _ => None,
     }
