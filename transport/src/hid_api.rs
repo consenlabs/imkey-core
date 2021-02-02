37432ed2595eee230954ef31a644323e8316c328
“fengweiqiang-coder” <weiqiang@imkey.im>
2021-02-02 11:39:29 +0800

    修改imkey里面显示币种的名称

--- transport/src/hid_api.rs
+++ transport/src/hid_api.rs
@@ -24,7 +24,7 @@ pub fn hid_send(apdu: &String, timeout: i32) -> Result<String> {
         drop(hid_device_obj);
         return Err(HidError::DeviceConnectInterfaceNotCalled.into());
     }
-    // println!("-->{}", apdu);
+    println!("-->{}", apdu);
     send_device_message(
         &hid_device_obj.get(0).unwrap(),
         Vec::from_hex(apdu.as_str()).unwrap().as_slice(),
@@ -32,7 +32,7 @@ pub fn hid_send(apdu: &String, timeout: i32) -> Result<String> {
     let return_data = read_device_response(&hid_device_obj.get(0).unwrap(), timeout)?;
     //    drop(hid_device_obj);
     let apdu_response = hex::encode_upper(return_data);
-    // println!("<--{}", apdu_response.clone());
+    println!("<--{}", apdu_response.clone());
     Ok(apdu_response)
 }
 
