225eabb765a8d0ae22630501b6387039cf7c5832
Neal Xu <imxuneal@gmail.com>
2021-03-17 11:15:55 +0800

    change the chain display name

--- common/src/applet.rs
+++ common/src/applet.rs
@@ -1,32 +1,32 @@
 pub fn get_appname_by_instid(instid: &str) -> Option<&str> {
     match instid {
-        "695F627463" => Some("BTC"),
-        "695F657468" => Some("ETH"),
+        "695F627463" => Some("Bitcoin"),
+        "695F657468" => Some("Ethereum"),
         "695F656F73" => Some("EOS"),
-        "695F636F736D6F73" => Some("COSMOS"),
-        "695F6B315F66696C" => Some("FILECOIN"),
-        "695F65645F6B736D" => Some("KUSAMA"),
-        "695F65645F646F74" => Some("POLKADOT"),
+        "695F636F736D6F73" => Some("Cosmos"),
+        "695F6B315F66696C" => Some("Filecoin"),
+        "695F65645F6B736D" => Some("Kusama"),
+        "695F65645F646F74" => Some("Polkadot"),
         "695F6B315F747278" => Some("TRON"),
-        "695F626368" => Some("BCH"),
-        "695F6C7463" => Some("LTC"),
+        "695F626368" => Some("Bitcion Cash"),
+        "695F6C7463" => Some("Litecoin"),
         "695F696D6B" => Some("IMK"),
         _ => None,
     }
 }
 pub fn get_instid_by_appname(appname: &str) -> Option<&str> {
     match appname {
-        "BTC" => Some("695F627463"),
-        "ETH" => Some("695F657468"),
+        "Bitcoin" => Some("695F627463"),
+        "Ethereum" => Some("695F657468"),
         "EOS" => Some("695F656F73"),
-        "COSMOS" => Some("695F636F736D6F73"),
-        "FILECOIN" => Some("695F6B315F66696C"),
-        "KUSAMA" => Some("695F65645F6B736D"),
-        "POLKADOT" => Some("695F65645F646F74"),
+        "Cosmos" => Some("695F636F736D6F73"),
+        "Filecoin" => Some("695F6B315F66696C"),
+        "Kusama" => Some("695F65645F6B736D"),
+        "Polkadot" => Some("695F65645F646F74"),
         "TRON" => Some("695F6B315F747278"),
-        "FILECOIN" => Some("4695F6B315F66696C"),
-        "BCH" => Some("695F626368"),
-        "LTC" => Some("695F6C7463"),
+        "Filecoin" => Some("4695F6B315F66696C"),
+        "Bitcion Cash" => Some("695F626368"),
+        "Litecoin" => Some("695F6C7463"),
         "IMK" => Some("695F696D6B"),
         _ => None,
     }
@@ -37,33 +37,33 @@ mod tests {
     use crate::applet::{get_appname_by_instid, get_instid_by_appname};
     #[test]
     fn get_appname_by_instid_test() {
-        assert_eq!(get_appname_by_instid("695F627463").unwrap(), "BTC");
-        assert_eq!(get_appname_by_instid("695F657468").unwrap(), "ETH");
+        assert_eq!(get_appname_by_instid("695F627463").unwrap(), "Bitcoin");
+        assert_eq!(get_appname_by_instid("695F657468").unwrap(), "Ethereum");
         assert_eq!(get_appname_by_instid("695F656F73").unwrap(), "EOS");
-        assert_eq!(get_appname_by_instid("695F636F736D6F73").unwrap(), "COSMOS");
+        assert_eq!(get_appname_by_instid("695F636F736D6F73").unwrap(), "Cosmos");
         assert_eq!(
             get_appname_by_instid("695F6B315F66696C").unwrap(),
             "FILECOIN"
         );
-        assert_eq!(get_appname_by_instid("695F626368").unwrap(), "BCH");
-        assert_eq!(get_appname_by_instid("695F6C7463").unwrap(), "LTC");
+        assert_eq!(get_appname_by_instid("695F626368").unwrap(), "Bitcoin");
+        assert_eq!(get_appname_by_instid("695F6C7463").unwrap(), "Litecoin");
         assert_eq!(get_appname_by_instid("695F696D6B").unwrap(), "IMK");
         assert!(get_appname_by_instid("1111111111").is_none());
     }
 
     #[test]
     fn get_instid_by_appname_test() {
-        assert_eq!(get_instid_by_appname("BTC").unwrap(), "695F627463");
-        assert_eq!(get_instid_by_appname("ETH").unwrap(), "695F657468");
+        assert_eq!(get_instid_by_appname("Bitcoin").unwrap(), "695F627463");
+        assert_eq!(get_instid_by_appname("Ethereum").unwrap(), "695F657468");
         assert_eq!(get_instid_by_appname("EOS").unwrap(), "695F656F73");
-        assert_eq!(get_instid_by_appname("COSMOS").unwrap(), "695F636F736D6F73");
+        assert_eq!(get_instid_by_appname("Cosmos").unwrap(), "695F636F736D6F73");
         assert_eq!(get_instid_by_appname("IMK").unwrap(), "695F696D6B");
         assert_eq!(
-            get_instid_by_appname("FILECOIN").unwrap(),
+            get_instid_by_appname("Filecoin").unwrap(),
             "695F6B315F66696C"
         );
-        assert_eq!(get_instid_by_appname("BCH").unwrap(), "695F626368");
-        assert_eq!(get_instid_by_appname("LTC").unwrap(), "695F6C7463");
+        assert_eq!(get_instid_by_appname("Bitcion Cash").unwrap(), "695F626368");
+        assert_eq!(get_instid_by_appname("Litecoin").unwrap(), "695F6C7463");
         assert!(get_instid_by_appname("APPLET").is_none());
     }
 }
