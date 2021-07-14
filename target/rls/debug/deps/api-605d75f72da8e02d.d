b0c8516e2f1cfa336e8f2f4861712a9b2433814c
Neal Xu <imxuneal@gmail.com>
2021-07-14 10:58:06 +0800

    chore: remove target from git

--- target/rls/debug/deps/api-605d75f72da8e02d.d
+++
@@ -1,5 +0,0 @@
-/Users/joe/work/rust_proj/imkey-core/target/rls/debug/deps/api-605d75f72da8e02d.rmeta: api/src/lib.rs
-
-/Users/joe/work/rust_proj/imkey-core/target/rls/debug/deps/api-605d75f72da8e02d.d: api/src/lib.rs
-
-api/src/lib.rs:
--- target/rls/debug/deps/device-68edff1a168b2a28.d
+++
@@ -1,5 +0,0 @@
-/Users/joe/work/rust_proj/imkey-core/target/rls/debug/deps/device-68edff1a168b2a28.rmeta: device/src/lib.rs
-
-/Users/joe/work/rust_proj/imkey-core/target/rls/debug/deps/device-68edff1a168b2a28.d: device/src/lib.rs
-
-device/src/lib.rs:
