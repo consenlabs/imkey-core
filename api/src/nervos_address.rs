ff2db9520fd2f0190d7665cf0dcfd41f945ba858
xiemener <xielamenla@qq.com>
2021-11-23 18:41:38 +0800

    fix: support ckb enc_xpub

--- api/src/nervos_address.rs
+++ api/src/nervos_address.rs
@@ -1,16 +1,27 @@
-use crate::api::{AddressParam, AddressResult};
+use crate::api::{AddressParam, AddressResult, BtcForkWallet};
 use crate::error_handling::Result;
 use crate::message_handler::encode_message;
+use bitcoin::Network;
+use coin_btc_fork::address::BtcForkAddress;
 use coin_ckb::address::CkbAddress;
 use prost::Message;
 
 pub fn get_address(param: &AddressParam) -> Result<Vec<u8>> {
-    let address = CkbAddress::get_address(param.network.as_ref(), param.path.as_ref())?;
+    let network = match param.network.as_ref() {
+        "MAINNET" => Network::Bitcoin,
+        "TESTNET" => Network::Testnet,
+        _ => Network::Testnet,
+    };
 
-    let address_message = AddressResult {
+    let address = CkbAddress::get_address(param.network.as_ref(), param.path.as_ref())?;
+    let account_path = common::utility::get_account_path(&param.path)?;
+    let enc_xpub = BtcForkAddress::get_enc_xpub(network, &account_path)?;
+
+    let address_message = BtcForkWallet {
         path: param.path.to_owned(),
         chain_type: param.chain_type.to_string(),
-        address,
+        address: address,
+        enc_x_pub: enc_xpub,
     };
     encode_message(address_message)
 }
@@ -24,4 +35,24 @@ pub fn display_address(param: &AddressParam) -> Result<Vec<u8>> {
         address,
     };
     encode_message(address_message)
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::api::AddressParam;
+    use crate::nervos_address::get_address;
+    use device::device_binding::bind_test;
+
+    #[test]
+    fn test_btc_fork_address() {
+        bind_test();
+
+        let param = AddressParam {
+            chain_type: "NERVOS".to_string(),
+            path: "m/44'/309'/0'/0/0".to_string(),
+            network: "MAINNET".to_string(),
+            is_seg_wit: false,
+        };
+        let message = get_address(&param);
+    }
 }
